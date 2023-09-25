080f07f2feb0d6ac3518cec6c5bbfcb6fe1a167e
twylatech <55984947+twylatech@users.noreply.github.com>
2023-09-25 12:10:45 +0400

    created frontend

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
-const express = require("express")
+const express = require("express");
+const cors = require("cors");
 const app = express()
 const PORT= 8080;
 
 app.use(express.json());
-
+app.use(cors());
 const db = require('./models');
 
 
